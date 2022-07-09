eaee981bd947ffd751008c851f36386987ea7b26
Berke YONCACI <berke5sekiz1@gmail.com>
2022-07-09 22:05:10 +0300

    add bundle analyzer plugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,19 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  const analyzerMode = process.env.INTERACTIVE_ANALYZE ? 'server' : 'json'
+
+  if (stage === 'build-javascript') {
+    actions.setWebpackConfig({
+      plugins: [
+        new BundleAnalyzerPlugin({
+          analyzerMode,
+        }),
+      ],
+    })
+  }
+}
+
 // const path = require('path')
 
 // exports.onCreateWebpackConfig = ({ actions }) => {
